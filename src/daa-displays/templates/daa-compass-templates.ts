b1389768cd600a7c904fbdc747be39b4f318a2f2
Paolo M. Masci <paolo.m.masci@nasa.gov>
2020-07-08 17:55:05 -0400

    improved position of compass labels

--- src/daa-displays/templates/daa-compass-templates.ts
+++ src/daa-displays/templates/daa-compass-templates.ts
@@ -1,6 +1,12 @@
 export const compassTemplate = `<div id="{{id}}-inner" style="position:absolute; top:{{top}}px; left:{{left}}px;">
     <!-- compass shade -->
     <div style="display:none; position:absolute; width:634px; height:634px; border-radius:400px; border: 79px solid #333333; position:absolute;opacity:0.6;"></div>
+    <!-- compass labels -->
+    <div id="{{id}}-compass-labels" style="position:absolute; opacity:0.8; color:white; display:none;">
+        <div id="{{id}}-compass-label-outer" style="position: absolute; left:546px; top:76px;">5</div>
+        <div id="{{id}}-compass-label-mid" style="position: absolute; left:435px; top:190px;">2.5</div>
+        <div id="{{id}}-compass-label-inner" style="position: absolute; left:378px; top:247px;">1.25</div>
+    </div>
     <!-- compass -->
     <div id="{{id}}-circle" style="position:absolute; width:634px; height:634px; transform-origin:center; transform:rotate(0deg);">
         <object data="{{baseUrl}}svgs/danti-quadrant.svg" type="image/svg+xml" style="width:635px;position:absolute;opacity:0.8;"></object>
@@ -13,12 +19,6 @@ export const compassTemplate = `<div id="{{id}}-inner" style="position:absolute;
         </div> -->
         <!-- compass bands -->
         <canvas id="{{id}}-bands" width="634" height="634" style="position:absolute; opacity:0.8;"></canvas>
-        <!-- compass labels -->
-        <div id="{{id}}-compass-labels" style="position:absolute; opacity:0.8; color:white; display:none;">
-            <div id="{{id}}-compass-label-outer" style="position: absolute; left:546px; top:76px;">5</div>
-            <div id="{{id}}-compass-label-mid" style="position: absolute; left:435px; top:190px;">2.5</div>
-            <div id="{{id}}-compass-label-inner" style="position: absolute; left:378px; top:247px;">1.25</div>
-        </div>
         <!-- resolution bug -->
         <div id="{{id}}-resolution-bug" style="position:absolute; width:634px; height:634px; transform:rotate(0deg); display:none;">
             <!-- indicator -->
