7930b15e3e5982623a530416ed39c554a24e2ac9
Cesar Munoz <cesar.a.munoz@nasa.gov>
2023-09-25 19:00:35 -0400

    Updated daidalus-submodules

---
+++ daidalus-submodules/add-release.sh
@@ -0,0 +1,7 @@
+if [ -z "$1" ]; then
+    echo "Release branch needs to be provided, e.g., v2.0.x"
+    exit
+fi
+git submodule add -b $1 https://github.com/nasa/daidalus.git $1
+git submodule update --init
+echo "Update releases, if needed, and then run: cd ..;make
